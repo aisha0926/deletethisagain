661d55e6041e2247124c2383ad587a4114b3f1a0
Aisha Amate <Aisha@Aishas-MacBook-Pro.local>
2018-06-28 17:01:54 +0100

    Updating

--- Week3_Intermediate/src/Garage/Car.java
+++ Week3_Intermediate/src/Garage/Car.java
@@ -2,16 +2,18 @@ package Garage;
 
 public class Car extends Vehicle {
 
+    public int numberOfDoors;
+
 
     public Car(int id){
         super(id);
 
     }
 
-    public Car(int id, String model, String colour){
+    public Car(int id, String model, String colour, String type){
         super(id);
         this.setModel(model);
         this.setColour(colour);
-        this.setType(VehicleType.HIGH_END);
+        this.setType(type);
     }
 }
--- Week3_Intermediate/src/Garage/VehicleType.java
+++ Week3_Intermediate/src/Garage/VehicleType.java
@@ -4,4 +4,5 @@ public class VehicleType {
     public static final String ECONOMY = "Economy";
     public static final String HIGH_END = "High End";
 
+
 }
